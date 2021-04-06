ddcbe37198c8b82492ab9733e5271b756252c5c3
eswarclynn <esuclintblazer@gmail.com>
2021-04-06 15:43:32 +0530

    Modfiy folder structure.

--- src/components/Thing/Thing.stories.tsx
+++ src/components/Thing/Thing.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Thing, Props } from '../src';
+import { Thing, Props } from './index';
 
 const meta: Meta = {
   title: 'Hello World',
@@ -23,8 +23,8 @@ const Template: Story<Props> = args => <Thing {...args} />;
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
-export const Default = Template.bind({});
+export const DefaultThing = Template.bind({});
 
-Default.args = {
+DefaultThing.args = {
   children: 'Welcome to 100ms',
 };
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { Thing } from './components/Thing';
