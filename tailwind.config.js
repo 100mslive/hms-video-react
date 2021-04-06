c8dbc4c7ea14da09092ea9ad273727bd51feeee6
Eswar Prasad Clinton. A <64120992+eswarclynn@users.noreply.github.com>
2021-04-06 16:35:28 +0530

    Add color scheme, typography. (#1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,38 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-const {
-  colors,
-  borderRadius,
-  lineHeight,
-} = require('tailwindcss/defaultTheme');
-
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        brand: {
-          100: '#6DA5FF',
-          250: '#438BFF',
-          300: '#2F80FF',
-          350: '#2A72E5',
-          400: '#2565CC',
-          500: '#1C4C99',
-          600: '#173F7F',
-          700: '#123266',
-          800: '#0E264C',
-          900: '#091933',
+        blue: {
+          tint: '#74AAFF',
+          main: '#2F80FF',
+          shade: '#0B326F',
+        },
+        red: {
+          tint: '#E66977',
+          main: '#D74451',
+          shade: '#6F2229',
         },
         gray: {
-          ...colors.gray,
-          150: '#FFFFFF66',
-          650: '#5c5c6e',
+          100: '#212121',
+          200: '#3B3B3B',
+          300: '#5E5E5E',
+          400: '#8E8E8E',
+          500: '#C7C7C7',
         },
-        dark: {
-          200: '#2B3139',
-          300: '#1B2028',
-          400: '#161B22',
-          500: '#0F141D',
-          600: '#0B0F15',
+        transparent: {
+          light: 'rgba(255, 255, 255, 0.25)',
+          dark: 'rgba(0, 0, 0, 0.75)',
+          disabled: 'rgba(59, 59, 59, 0.3)',
         },
+      },
+      fontSize: {
+        sm: ['0.75rem', { lineHeight: '1rem' }],
+        base: ['0.875rem', { lineHeight: '1.25rem' }],
+        lg: ['1rem', { lineHeight: '1.5rem' }],
+        xl: ['1.125rem', { lineHeight: '1.5rem' }],
+        '2xl': ['1.5rem', { lineHeight: '1.75rem' }],
       },
     },
   },
