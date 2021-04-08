cca1184670761fb0de3641069588e6f5e464e268
nikhilbghodke <nikhil.ghodke17@vit.edu>
2021-04-08 20:48:22 +0530

    added initial theming

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,8 @@
 import '../src/index.css';
 
+import { addons } from '@storybook/addons';
+import { themes } from '@storybook/theming';
+
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
