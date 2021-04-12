fb27ab96d69d3df0b07a7f63f8fdfc30340ef74f
Tushar Tripathi <tushutripathi@gmail.com>
2021-04-12 14:56:02 +0530

    build: add more eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 // These rules are merged into the out of box rules coming from tsdx - https://github.com/formium/tsdx/blob/master/src/createEslintConfig.ts
 module.exports = {
     "rules": {
-        "complexity": ["error", 4],
-        curly: ["error", "all"]
+        "complexity": ["error", 6],  // if a component/function is getting too complex it should be broken down
+        "curly": ["error", "all"],   // use curly brace even for single line functions
+        "comma-dangle": ["error", "always-multiline"],  // easier to see git diff
+        "prefer-template": ["error"],                   // easier to read code when variables are used
+        "react/prefer-stateless-function": ["error"]    // functional components are better
     }
 }
