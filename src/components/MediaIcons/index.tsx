8c0241c295038335f37cbe7fd65305b7ba6925dd
grtsid <siddhantagarwal99@gmail.com>
2021-04-14 15:42:59 +0530

    added header and control bar

--- src/components/MediaIcons/index.tsx
+++ src/components/MediaIcons/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { MicOff, MicOn } from '../../icons';
+import { MicOff, MicOn, CamOff, CamOn, Logo } from '../../icons';
+
+
+export const LogoButton = () => {
+  return (
+    <button className="inline-block p-2 focus:outline-none">{Logo}</button>
+  );
+};
 
 export const AudioMuteButton = ({ isAudioMuted = false }) => {
   return (
@@ -14,6 +21,21 @@ export const AudioMuteButton = ({ isAudioMuted = false }) => {
     </button>
   );
 };
+
+export const VideoMuteButton = ({ isVideoMuted = false }) => {
+  return (
+    <button
+      className={`inline-block p-2 rounded-lg focus:outline-none ${
+        isVideoMuted
+          ? 'bg-red-main hover:bg-red-tint'
+          : 'hover:bg-transparent-light'
+      }`}
+    >
+      {isVideoMuted ? CamOff : CamOn}
+    </button>
+  );
+};
+
 
 export const AudioMuteIndicator = ({
   isAudioMuted = false,
