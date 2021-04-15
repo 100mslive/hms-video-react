e868c385f96528dbfb60f7f10af82bae2b4a7825
nikhilbghodke <nikhil.ghodke17@vit.edu>
2021-04-15 15:19:35 +0530

    publish npm:exported every component from index.tsx

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export { Thing } from './components/Thing';
+export { VideoTile, VideoTileProps } from './components/VideoTile/index';
+export { Avatar } from './components/Avatar/index';
+export { VideoList, VideoListProps } from './components/VideoList/index';
+export * from './components/MediaIcons/index';
