b0ca13e13f7b998a4a7c9aceae545354443a95d7
Aniket Behera <aniket.behera@gmail.com>
2021-05-04 18:44:35 +0530

    feat: Moving settings to twind

--- src/components/ParticipantList/ParticipantList.tsx
+++ src/components/ParticipantList/ParticipantList.tsx
@@ -5,6 +5,7 @@ import { Participant } from '../../types';
 import { AvatarList } from '../Avatar';
 import Popover from '@material-ui/core/Popover';
 import { withClasses } from '../../utils/styles';
+import { combineClasses} from '../../utils';
 //@ts-ignore
 import { create } from 'twind';
 
@@ -44,7 +45,7 @@ const defaultClasses:ParticipantListClasses = {
   menuIconContainer:'flex flex-grow justify-end right-0 absolute'
 }
 
-export const StyledParticipantList = ({ participantList, defaultClasses, classes }: StyledParticipantListProps) => {
+export const StyledParticipantList = ({ participantList, defaultClasses, classes:extraClasses }: StyledParticipantListProps) => {
   //@ts-expect-error
   const combinedClasses = combineClasses(defaultClasses, extraClasses);
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -67,12 +68,12 @@ export const StyledParticipantList = ({ participantList, defaultClasses, classes
       <button
         aria-describedby={id}
         type="button"
-        className={` ${combinedClasses?.root}
-          ${open ? combinedClasses?.buttonOpen : combinedClasses.buttonClosed
+        className={` ${combinedClasses?.buttonRoot}
+          ${open ? combinedClasses?.buttonOpen : combinedClasses?.buttonClosed
         }`}
         onClick={handleClick}
       >
-        <div className={`${combinedClasses?.buttonInnter}`}>
+        <div className={`${combinedClasses?.buttonInner}`}>
           {participantList.length} in room
           <span className={`${combinedClasses?.buttonText}`}>
             {open ? UpCarret : DownCarret}
@@ -99,7 +100,6 @@ export const StyledParticipantList = ({ participantList, defaultClasses, classes
               <div key={index}>
                 <div role="none" key={role}>
                   <a
-                    href="#"
                     className={`${combinedClasses?.menuSection}`}
                     role="menuitem"
                     tabIndex={-1}
@@ -138,8 +138,81 @@ export const StyledParticipantList = ({ participantList, defaultClasses, classes
 
 export type ParticipantListProps = Omit<StyledParticipantListProps, 'defaultClasses'>;
 
+//TODO replace with themeContext
 export const ParticipantList = withClasses<ParticipantListClasses | undefined>(
   defaultClasses,
   'participantList',
-  create().tw,
+  create({
+    theme:{
+      extend: {
+        transitionProperty: {
+          height: 'height',
+        },
+        colors: {
+          blue: {
+            tint: '#74AAFF',
+            main: '#2F80FF',
+            shade: '#0B326F',
+          },
+          red: {
+            tint: '#E66977',
+            main: '#D74451',
+            shade: '#6F2229',
+          },
+          gray: {
+            100: '#212121',
+            200: '#3B3B3B',
+            300: '#5E5E5E',
+            400: '#8E8E8E',
+            500: '#C7C7C7',
+          },
+          transparent: {
+            light: 'rgba(255, 255, 255, 0.25)',
+            dark: 'rgba(0, 0, 0, 0.75)',
+            disabled: 'rgba(59, 59, 59, 0.3)',
+          },
+        },
+        fontSize: {
+          sm: ['0.75rem', { lineHeight: '1rem' }],
+          base: ['0.875rem', { lineHeight: '1.25rem' }],
+          lg: ['1rem', { lineHeight: '1.5rem' }],
+          xl: ['1.25rem', { lineHeight: '1.75rem' }],
+          '2xl': ['1.5rem', { lineHeight: '1.75rem' }],
+        },
+        fontFamily: {
+          inter: ['Inter'],
+        },
+        maxHeight: {
+          100: '37.5rem',
+          116: '29rem',
+        },
+        width: {
+          7.5: '1.875rem',
+          8.75: '8.75rem',
+          22.5: '22.5rem',
+          37.5: '37.5rem',
+          42.5: '42.5rem',
+          100: '32.25rem',
+        },
+        height: {
+          3.25: '3.25rem',
+          22.5: '22.5rem',
+          37.5: '37.5rem',
+          400: '40rem',
+          42.5: '42.5rem',
+        },
+        scale: {
+          '-100': '-1',
+        },
+        margin: {
+          1.625: '1.625rem',
+          1.875: '1.875rem',
+          5.5: '5.5rem',
+        },
+        padding: {
+          0.875: '0.875rem',
+        },
+      },
+    }
+  }).tw,
 )<StyledParticipantListProps>(StyledParticipantList);
