ac5530e19f5e7b70f06073792431d386d0b36123
Aniket Behera <aniket.behera@gmail.com>
2021-05-04 16:57:30 +0530

    fix: Removed dominant speaker changes as they causd an import issue

--- src/hooks/helpers/createListener.ts
+++ src/hooks/helpers/createListener.ts
@@ -11,12 +11,11 @@ import HMSUpdateListener, {
 import HMSTrack from '@100mslive/100ms-web-sdk/dist/media/tracks/HMSTrack';
 
 const createListener = (
-  sdk: HMSSdk,
   incomingListener: HMSUpdateListener,
   setPeers: React.Dispatch<React.SetStateAction<HMSPeer[]>>,
   setLocalPeer: React.Dispatch<React.SetStateAction<HMSPeer>>,
   receiveMessage: (message: HMSMessage) => void,
-  setDominantSpeaker: React.Dispatch<React.SetStateAction<HMSPeer | null>>,
+  sdk: HMSSdk,
 ) => {
   const myListener = {
     onJoin: (room: HMSRoom) => {
@@ -31,37 +30,26 @@ const createListener = (
       const peers = sdk.getPeers();
       console.debug(
         'HMSui-component: Listener [onPeerUpdate]',
-        HMSPeerUpdate[type],
         peer,
         { peers },
       );
 
       setPeers(peers);
       setLocalPeer(sdk.getLocalPeer());
-      if (type === HMSPeerUpdate.BECAME_DOMINANT_SPEAKER) {
-        setDominantSpeaker(peer);
-      }
-      if (type === HMSPeerUpdate.RESIGNED_DOMINANT_SPEAKER) {
-        setDominantSpeaker(null);
-      }
       incomingListener.onPeerUpdate(type, peer);
     },
 
     onRoomUpdate: (type: HMSRoomUpdate, room: HMSRoom) => {
       console.debug(
-        'HMSui-component: Listener [onRoomUpdate]',
-        HMSRoomUpdate[type],
-        room,
-        { peers: sdk.getPeers() },
+        'HMSui-component: [onRoomUpdate] Inside listener, peers are',
+        sdk.getPeers(),
       );
-      incomingListener.onRoomUpdate(type, room);
     },
 
     onTrackUpdate: (type: HMSTrackUpdate, track: HMSTrack, peer: HMSPeer) => {
       const peers = sdk.getPeers();
       console.debug(
         'HMSui-component: Listener [onTrackUpdate]',
-        HMSTrackUpdate[type],
         track,
         peer,
         { peers },
