a51b0de532a7255a73de38118dc949283615f48d
nikhilbghodke <nikhil.ghodke17@vit.edu>
2021-05-04 18:22:04 +0530

    chore: preview convert to twind

--- src/components/ChatBox/ChatBox.tsx
+++ src/components/ChatBox/ChatBox.tsx
@@ -251,29 +251,5 @@ export type ChatProps = Omit<StyledChatProps, 'defaultClasses'>;
 export const ChatBox = withClasses<ChatBoxClasses | undefined>(
   defaultClasses,
   'chatBox',
-  create({
-    theme: merge(theme, {
-      extend: {
-        colors: {
-          blue: {
-            tint: '#74AAFF',
-            main: '#2F80FF',
-            shade: '#0B326F',
-          },
-          red: {
-            tint: '#E66977',
-            main: '#D74451',
-            shade: '#6F2229',
-          },
-          gray: {
-            100: '#212121',
-            200: '#3B3B3B',
-            300: '#5E5E5E',
-            400: '#8E8E8E',
-            500: '#C7C7C7',
-          },
-        },
-      },
-    }),
-  }).tw,
+  create().tw,
 )<StyledChatProps>(StyledChatBox);
