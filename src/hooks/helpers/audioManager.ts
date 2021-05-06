fa8f50162842c16868e76774b69e1e9f54554d53
aniketbehera <aniket.behera@gmail.com>
2021-05-07 04:41:27 +0530

    chore: Merge audio fix (#76)
    * fix: TS error in font
    
    * feat: Adds audio manager
    
    * feat: Removes audio from inside video tile
    
    * feat: Adds audio manager

---
+++ src/hooks/helpers/audioManager.ts
@@ -0,0 +1,20 @@
+export const initAudioSink = () => {
+    const audioSink = document.createElement("div");
+    // TODO rename random
+    audioSink.id = "audio-sink";
+    document.body.append(audioSink);
+}
+
+export const addAudioTrack = ({track}:{track:MediaStreamTrack}) => {
+    const audioEl = document.createElement("audio");
+    audioEl.autoplay = true;
+    audioEl.style.display = "none";
+    audioEl.id = track.id;
+    audioEl.srcObject = new MediaStream([track]);
+    // TODO is this even needed?
+    document.getElementById('audio-sink')?.append(audioEl);    
+}
+
+export const removeAudioTrack = ({track}:{track:MediaStreamTrack}) => {
+    document.getElementById(track.id)?.remove();
+}
