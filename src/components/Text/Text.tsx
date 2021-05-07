511c0e524749904b58d58cd8cb0a84c2d466636d
Deepankar Bhade <deep14bhade@gmail.com>
2021-05-07 12:59:10 +0530

    feat: tag props to typography

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,4 +1,20 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
+
+type TextTags =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'p'
+  | 'b'
+  | 'small'
+  | 'i'
+  | 'span'
+  | 'del'
+  | 'em'
+  | 'blockquote';
 
 interface StyledTextProps {
   /**
@@ -9,14 +25,30 @@ interface StyledTextProps {
    * Size
    */
   size?: 'sm' | 'md' | 'lg';
+  /**
+   * HTML Tag to Render
+   */
+  tag?: TextTags;
 }
 
 export interface TextClasses {}
 
 const defaultClasses = {};
 
-const Text = () => {
-  return <p>Text Component</p>;
+type NativeAttrs = Omit<
+  React.DetailsHTMLAttributes<any>,
+  keyof StyledTextProps
+>;
+export type TextProps = StyledTextProps & NativeAttrs;
+
+const Text: React.FC<PropsWithChildren<TextProps>> = ({
+  tag,
+  variant,
+  size,
+  children,
+}) => {
+  const TagName = tag || 'p';
+  return <TagName>{children}</TagName>;
 };
 
 export default Text;
