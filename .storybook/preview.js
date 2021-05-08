078934e5624ee0a16975be05b2b89880c6b3b081
Eswar Prasad Clinton. A <64120992+eswarclynn@users.noreply.github.com>
2021-05-08 21:31:18 +0530

    Feat: Add endpoint field and accept initial values in join. (#84)
    * feat: Add endpoint field, accept initial values.
    
    * fix: Change storybook input bg to black.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,5 @@
 import '../src/index.css';
 
-import { addons } from '@storybook/addons';
-import { themes } from '@storybook/theming';
-
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
