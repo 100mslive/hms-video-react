c01b46095d74687f9141c1b03a72897dc9a0919f
Deepankar Bhade <deep14bhade@gmail.com>
2021-05-08 20:44:41 +0530

    refactor: adding combineClasses in avatar

--- src/components/TwAvatar/Avatar.tsx
+++ src/components/TwAvatar/Avatar.tsx
@@ -1,5 +1,6 @@
 import React, { PropsWithChildren } from 'react';
-import { getInitialsFromName } from '../../utils';
+import { getInitialsFromName, combineClasses } from '../../utils';
+import { withClasses } from '../../utils/styles';
 
 interface StyledAvatarProps {
   /**
@@ -23,68 +24,79 @@ interface StyledAvatarProps {
    */
   size?: 'sm' | 'md' | 'lg' | 'xl';
   /**
-   * className string
+   * Default class names
    */
-  className?: string;
+  defaultClasses?: AvatarClasses;
+  /**
+   * Extra class names
+   */
+  classes?: AvatarClasses;
 }
 
-type NativeAttrs = Omit<
-  React.DetailsHTMLAttributes<any>,
-  keyof StyledAvatarProps
->;
-export type AvatarProps = StyledAvatarProps & NativeAttrs;
+export interface AvatarClasses {
+  root: string;
+  rootSizeSm: string;
+  rootSizeMd: string;
+  rootSizeLg: string;
+  rootSizeXl: string;
+  rootShapeCircle: string;
+  rootShapeSquare: string;
+  rootDivWrapper: string;
+}
 
-const defaultClasses = {
-  size: {
-    sm: 'w-8 h-8 ',
-    md: 'w-12 h-12',
-    lg: 'w-16 h-16 text-xl',
-    xl: 'w-28 h-28 text-4xl',
-  },
-  shape: {
-    circle: 'rounded-full',
-    square: 'rounded-lg',
-  },
-  type: {
-    initial: 'flex text-center items-center justify-center',
-  },
+const defaultClasses: AvatarClasses = {
+  root: 'object-cover',
+  rootSizeSm: 'w-8 h-8 ',
+  rootSizeMd: 'w-12 h-12',
+  rootSizeLg: 'w-16 h-16 text-xl',
+  rootSizeXl: 'w-28 h-28 text-4xl',
+  rootShapeCircle: 'rounded-full',
+  rootShapeSquare: 'rounded-lg',
+  rootDivWrapper: 'flex text-center items-center justify-center',
 };
 
-export const Avatar: React.FC<PropsWithChildren<AvatarProps>> = ({
-  size,
+export const StyledAvatar: React.FC<PropsWithChildren<StyledAvatarProps>> = ({
+  size = 'sm',
   label,
   icon,
   image,
-  shape,
-  className,
+  shape = 'circle',
+  defaultClasses,
+  classes: extraClasses,
   ...props
 }) => {
-  const resolveClasses = (
-    size: StyledAvatarProps['size'],
-    shape: StyledAvatarProps['shape'],
-  ) => {
-    const tempSize = size || 'sm';
-    const tempShape = shape || 'circle';
-    return `${defaultClasses['size'][tempSize]} ${defaultClasses['shape'][tempShape]}`;
-  };
+  //@ts-expect-error
+  const combinedClasses = combineClasses(defaultClasses, extraClasses);
   const tempDisplay = image || getInitialsFromName(label);
-  const defaultClassNames = `${resolveClasses(
-    size,
-    shape,
-  )} inline object-cover ${className || ''}`;
+  const classList = [`${combinedClasses?.root}`];
+  shape === 'circle'
+    ? classList.push(`${combinedClasses?.rootShapeCircle}`)
+    : classList.push(`${combinedClasses?.rootShapeSquare}`);
+  if (!image) {
+    classList.push(`${combinedClasses?.rootDivWrapper}`);
+  }
+  if (size === 'sm') {
+    classList.push(`${combinedClasses?.rootSizeSm}`);
+  } else if (size === 'md') {
+    classList.push(`${combinedClasses?.rootSizeMd}`);
+  } else if (size === 'lg') {
+    classList.push(`${combinedClasses?.rootSizeLg}`);
+  } else if (size === 'xl') {
+    classList.push(`${combinedClasses?.rootSizeXl}`);
+  }
   return (
     <>
       {image ? (
         <img
           {...props}
-          className={defaultClassNames}
+          className={classList.join(' ')}
           src={image}
           alt="Profile image"
         />
       ) : (
         <div
           {...props}
-          className={`${defaultClassNames} ${defaultClasses['type']['initial']}`}
+          className={classList.join(' ')}
           style={{
             backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(
               16,
@@ -97,3 +109,16 @@ export const Avatar: React.FC<PropsWithChildren<AvatarProps>> = ({
     </>
   );
 };
+
+type NativeAttrs = Omit<
+  React.DetailsHTMLAttributes<any>,
+  keyof StyledAvatarProps
+>;
+
+export type AvatarProps = Omit<StyledAvatarProps, 'defaultClasses'> &
+  NativeAttrs;
+
+export const Avatar = withClasses<AvatarClasses | undefined>(
+  defaultClasses,
+  'avatar',
+)<StyledAvatarProps>(StyledAvatar);
