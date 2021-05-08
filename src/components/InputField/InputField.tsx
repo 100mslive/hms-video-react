29678418c3725b38b368930ab0ff1a4566849eab
Deepankar Bhade <deep14bhade@gmail.com>
2021-05-08 19:48:24 +0530

    refactor: adding combineClasses in inputfield

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-
-interface StyledInputField {
+import React, { MouseEventHandler, PropsWithChildren } from 'react';
+import { combineClasses } from '../../utils';
+import { withClasses } from '../../utils/styles';
+// TODO add a way to send styles
+interface StyledInputFieldProps {
   /**
    * Smaller Width for InputField
    */
@@ -14,44 +16,80 @@ interface StyledInputField {
    */
   placeHolder?: string;
   /**
-   * className string
+   * Default class names
    */
-  className?: string;
+  defaultClasses?: InputFieldClasses;
+  /**
+   * Extra class names
+   */
+  classes?: InputFieldClasses;
 }
 
-type NativeAttrs = Omit<
-  React.DetailsHTMLAttributes<any>,
-  keyof StyledInputField
->;
-export type InputFieldProps = StyledInputField &
-  NativeAttrs &
-  JSX.IntrinsicElements['input'];
+export interface InputFieldClasses {
+  root?: string;
+  rootCompact?: string;
+  rootNonCompact?: string;
+  rootRing?: string;
+  rootValidationRing?: string;
+  rootValidationText?: string;
+}
 
-const defaultClasses = {
-  common: `placeholder-blueGray-300 relative bg-gray-200 text-white text-lg rounded-lg border-0 shadow outline-none focus:outline-none focus:ring w-full`,
-  validation: `text-red-main mt-2`,
+const defaultClasses: InputFieldClasses = {
+  root:
+    'placeholder-blueGray-300 relative bg-gray-200 text-white text-lg rounded-lg border-0 shadow outline-none focus:outline-none focus:ring w-full',
+  rootCompact: `py-2 px-1`,
+  rootNonCompact: `p-3`,
+  rootRing: `ring-blue-300`,
+  rootValidationRing: `ring-red-main`,
+  rootValidationText: `text-red-main mt-2`,
 };
 
-export const InputField: React.FC<InputFieldProps> = ({
+export const StyledInputField: React.FC<PropsWithChildren<
+  StyledInputFieldProps
+>> = ({
   compact,
   validation,
   placeHolder,
-  className,
+  defaultClasses,
+  classes: extraClasses,
+  children,
   ...props
 }) => {
+  //@ts-expect-error
+  const combinedClasses = combineClasses(defaultClasses, extraClasses);
+  const classList: string[] = [`${combinedClasses?.root}`];
+  compact
+    ? classList.push(`${combinedClasses?.rootCompact}`)
+    : classList.push(`${combinedClasses?.rootNonCompact}`);
+  validation
+    ? classList.push(`${combinedClasses?.rootValidationRing}`)
+    : classList.push(`${combinedClasses?.rootRing}`);
   return (
-    <div>
+    <>
       <input
-        type="text"
         placeholder={placeHolder}
-        className={`${defaultClasses['common']} ${
-          validation ? 'ring-red-main' : 'ring-blue-300'
-        } ${compact ? 'py-1 px-2' : 'p-3'} ${className}`}
+        className={`${classList.join(' ')}`}
         {...props}
-      />
+      >
+        {children}
+      </input>
       {validation && (
-        <p className={defaultClasses['validation']}>{validation}</p>
+        <p className={`${defaultClasses?.rootValidationText}`}>{validation}</p>
       )}
-    </div>
+    </>
   );
 };
+
+type NativeAttrs = Omit<
+  React.DetailsHTMLAttributes<any>,
+  keyof StyledInputFieldProps
+> &
+  JSX.IntrinsicElements['input'];
+
+export type InputFieldProps = Omit<StyledInputFieldProps, 'defaultClasses'> &
+  NativeAttrs;
+
+export const InputField = withClasses<InputFieldClasses | undefined>(
+  defaultClasses,
+  'input',
+)<StyledInputFieldProps>(StyledInputField);
