b724a68a0d0d5c25a06903f4963b49a59e6bbf4f
Deepankar Bhade <deep14bhade@gmail.com>
2021-05-10 08:38:49 +0530

    chore: adding storybook examples

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { Text, TextProps } from './index';
+import { css } from 'twind/css';
 
 const meta: Meta = {
   title: 'Typography',
@@ -17,5 +18,39 @@ const Basic: Story<TextProps> = args => {
   );
 };
 
+const Class: Story<TextProps> = args => {
+  return (
+    <div className="w-full h-1/2 flex justify-center bg-white py-4">
+      <Text className="font-bold text-red-800 text-7xl" {...args}>
+        Override By ClassName
+      </Text>
+    </div>
+  );
+};
+
+const Style: Story<TextProps> = args => {
+  const styles = css`
+    color: purple;
+    font-size: 4rem;
+  `;
+  return (
+    <div className="w-full h-1/2 flex justify-center bg-white py-4">
+      <Text
+        styles={styles}
+        className="font-bold text-red-800 text-5xl"
+        {...args}
+      >
+        Override By Styles Props
+      </Text>
+    </div>
+  );
+};
+
 export const Default = Basic.bind({});
+Basic.args = {};
+
+export const ClassOverride = Class.bind({});
+Basic.args = {};
+
+export const StyleOverride = Style.bind({});
 Basic.args = {};
