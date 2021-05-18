968f96b7de28ed4c74f9da97bcadd92e36276631
Aniket Behera <aniket.behera@gmail.com>
2021-05-18 15:50:13 +0530

    feat: Minor styling fixes

--- src/components/VerticalDivider/VerticalDivider.tsx
+++ src/components/VerticalDivider/VerticalDivider.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useHMSTheme } from '../../hooks/HMSThemeProvider';
 
 export const VerticalDivider = () => {
-  return <div className="w-px bg-gray-200 h-6"></div>;
+  const {tw} = useHMSTheme();
+  return <div className={`${tw('w-px bg-gray-200 h-6')}`}></div>;
 };
