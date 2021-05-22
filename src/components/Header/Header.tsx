46d4eae5b4199bc615bfe58aa44597b920ddb86e
Aniket Behera <aniket.behera@gmail.com>
2021-05-22 17:42:23 +0530

    fix(Input, Header): Remove twind callback error

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
-import React, { useCallback } from 'react';
+import React, { useMemo } from 'react';
 import { VolumeIcon, Logo } from '../Icons';
 import { hmsUiClassParserGenerator } from '../../utils/classes';
+import {useHMSTheme} from '../../hooks/HMSThemeProvider'
 
 export interface HeaderClasses {
   root?: string;
@@ -49,20 +50,20 @@ export const Header = ({
   rightComponents = [],
   classes,
 }: HeaderProps) => {
-  const hu = useCallback(
+  const {tw} = useHMSTheme();
+  const styler = useMemo(()=>
     hmsUiClassParserGenerator<HeaderClasses>({
+      tw,
       classes,
       defaultClasses,
       tag: 'hmsui-header',
-    }),
-    [],
-  );
+    }),[]);
 
   return (
-    <div className={hu('root')}>
-      <div className={hu('leftRoot')}>{leftComponents}</div>
-      <div className={hu('centerRoot')}>{centerComponents}</div>
-      <div className={hu('rightRoot')}>{rightComponents}</div>
+    <div className={styler('root')}>
+      <div className={styler('leftRoot')}>{leftComponents}</div>
+      <div className={styler('centerRoot')}>{centerComponents}</div>
+      <div className={styler('rightRoot')}>{rightComponents}</div>
     </div>
   );
 };
