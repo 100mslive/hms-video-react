72a288b73522ce7bc77fe94e95a6b8a431c25f39
Deepankar Bhade <deep14bhade@gmail.com>
2021-05-25 07:22:51 +0530

    fix: spacing in 1st person display

--- src/components/FirstPersonDisplay/FirstPersonDisplay.tsx
+++ src/components/FirstPersonDisplay/FirstPersonDisplay.tsx
@@ -9,17 +9,22 @@ const FirstPersonDisplay: React.FC<Props> = ({ username = 'Sanjana' }) => {
   return (
     <div className="h-full text-white flex items-center justify-center">
       <div
-        className="w-37.5 h-42.5 rounded-2xl relative flex flex-col text-center space-y-6 justify-center items-center"
+        className="w-37.5 h-42.5 rounded-2xl relative flex flex-col text-center space-y-4 pt-44 items-center"
         style={{
           backgroundImage: `url(https://i.postimg.cc/SKvM5DY1/back.png)`,
         }}
       >
         <Text tag="h1" variant="heading">
-          Welcome {username} !
+          Welcome , {username} !
         </Text>
-        <Text tag="p" variant="heading" size="sm">
-          You’re the first one here. Sit back and relax till the others join.
-        </Text>
+        <div>
+          <Text tag="p" variant="heading" size="sm">
+            You’re the first one here.
+          </Text>
+          <Text tag="p" variant="heading" size="sm">
+            Sit back and relax till the others join.
+          </Text>
+        </div>
       </div>
     </div>
   );
