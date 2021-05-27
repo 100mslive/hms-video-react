03d60125ad77531758a0a1c93282d4e46c6566b4
nikhilbghodke <nikhil.ghodke17@vit.edu>
2021-05-27 19:26:56 +0530

    fix: changing method name

--- src/storybook/store/StoryBookSDK.ts
+++ src/storybook/store/StoryBookSDK.ts
@@ -20,7 +20,7 @@ export class StoryBookSDK implements IHMSBridge {
   constructor(store: IHMSStore) {
     this.store = store;
   }
-  markMessage(id: string, readStatus: boolean): void {
+  setMessageRead(id: string, readStatus: boolean): void {
     return;
   }
 
