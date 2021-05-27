1bca8051273ef05a969cd772d21fe4661eaea181
Tushar <tushar@100ms.live>
2021-05-27 10:16:10 +0530

    fix: pass peer to showscreensharefn

--- src/utils/videoListUtils.ts
+++ src/utils/videoListUtils.ts
@@ -1,25 +1,26 @@
-import { HMSPeer, HMSPeerID, HMSTrack, HMSTrackID } from '../store/schema';
+import { HMSPeer, HMSTrack, HMSTrackID } from '../store/schema';
 
-export type TrackWithPeer = {track: HMSTrack, peer: HMSPeer};
+export type TrackWithPeer = { track: HMSTrack; peer: HMSPeer };
 
-export const getVideoTracksFromPeers = (peers: HMSPeer[],
-                                        tracks: Record<HMSTrackID, HMSTrack>,
-                                        showScreenFn: (peerID: HMSPeerID) => boolean) => {
+export const getVideoTracksFromPeers = (
+  peers: HMSPeer[],
+  tracks: Record<HMSTrackID, HMSTrack>,
+  showScreenFn: (peer: HMSPeer) => boolean,
+) => {
   if (!peers || !tracks || !showScreenFn) {
     return [];
   }
   const videoTracks: TrackWithPeer[] = [];
   for (let peer of peers) {
     if (peer.videoTrack && tracks[peer.videoTrack]) {
-      videoTracks.push({track: tracks[peer.videoTrack], peer: peer});
+      videoTracks.push({ track: tracks[peer.videoTrack], peer: peer });
     }
-    if (showScreenFn(peer.id) && peer.auxiliaryTracks.length > 0) {
+    if (showScreenFn(peer) && peer.auxiliaryTracks.length > 0) {
       const screenShareTrackID = peer.auxiliaryTracks[0];
       if (tracks[screenShareTrackID]) {
-        videoTracks.push({track: tracks[screenShareTrackID], peer: peer});
+        videoTracks.push({ track: tracks[screenShareTrackID], peer: peer });
       }
     }
   }
   return videoTracks;
-}
-
+};
