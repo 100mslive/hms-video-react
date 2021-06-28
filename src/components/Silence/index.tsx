dabcecaa9aa5e6f971018cd0e588174233bbe1d3
raviteja83 <ravi.theja@100ms.live>
2021-06-28 10:32:23 +0530

    fix: use 100ms.live url for silence

--- src/components/Silence/index.tsx
+++ src/components/Silence/index.tsx
@@ -3,10 +3,6 @@ import React from 'react';
 //TODO replace with unpkg
 export const Silence = () => {
   return (
-    <audio
-      src="https://100ms-website-100mslive.vercel.app//silence.mp3"
-      autoPlay
-      className="hidden"
-    />
+    <audio src="https://100ms.live/silence.mp3" autoPlay className="hidden" />
   );
 };
