4ad1cbe6e68422765f0aed8f6fb5b140c52d7aab
eswarclynn <esuclintblazer@gmail.com>
2021-08-02 16:33:52 +0530

    fix: Use single storybook store, remove expired demo videos

--- src/storybook/store/SetUpFakeStore.ts
+++ src/storybook/store/SetUpFakeStore.ts
@@ -5,14 +5,10 @@ import { HMSReactiveStore, HMSStore } from '@100mslive/hms-video-store';
 import create from 'zustand';
 
 const store = HMSReactiveStore.createNewHMSStore();
-export const storyBookStore = create<HMSStore>(
-  HMSReactiveStore.createNewHMSStore(),
-);
+export const storyBookStore = create<HMSStore>(store);
 export const storyBookSDK = new StoryBookSDK(store);
 
 const videoURLS = [
-  'https://res.cloudinary.com/dlzh3j8em/video/upload/v1618618376/Screen_Recording_2021-04-17_at_5.36.24_AM_if70nz_wl31nt.mp4',
-  'https://res.cloudinary.com/dlzh3j8em/video/upload/v1618618246/pexels-mart-production-7261921_XCEC2bNM_osJG_lhdtua.mp4',
   'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
   'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4',
   'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4',
