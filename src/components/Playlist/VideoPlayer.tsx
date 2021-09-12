477b03ed2c9b20515d6483ffce5942359c5e03fd
raviteja83 <ravi.theja@100ms.live>
2021-09-12 15:06:27 +0530

    fix: handle close on video player

--- src/components/Playlist/VideoPlayer.tsx
+++ src/components/Playlist/VideoPlayer.tsx
@@ -4,10 +4,11 @@ import {
   HMSPlaylistType,
   selectPlaylistCurrentSelection,
   selectPlaylistVideoTrackByPeerID,
+  selectPlaylistAudioTrackByPeerID,
 } from '@100mslive/hms-video-store';
 import { PlaylistControls } from './PlaylistControls';
 import { Video } from '../Video/Video';
-import { useHMSStore } from '../../hooks/HMSRoomProvider';
+import { useHMSActions, useHMSStore } from '../../hooks/HMSRoomProvider';
 import { useHMSTheme } from '../../hooks/HMSThemeProvider';
 import { hmsUiClassParserGenerator } from '../../utils/classes';
 import { Text } from '../Text';
@@ -46,9 +47,11 @@ export const VideoPlayer = ({ classes, peer }: VideoPlayerProps) => {
     [classes],
   );
   const videoTrack = useHMSStore(selectPlaylistVideoTrackByPeerID(peer.id));
+  const audioTrack = useHMSStore(selectPlaylistAudioTrackByPeerID(peer.id));
   const currentVideo = useHMSStore(
     selectPlaylistCurrentSelection(HMSPlaylistType.video),
   );
+  const hmsActions = useHMSActions();
   const rootRef = useRef<HTMLDivElement>(null);
   const [isFullScreen, setIsFullScreen] = useState(false);
 
@@ -78,7 +81,18 @@ export const VideoPlayer = ({ classes, peer }: VideoPlayerProps) => {
             variant="no-fill"
             size="sm"
             shape="rectangle"
-            onClick={() => {}}
+            onClick={async () => {
+              await hmsActions.playlist.pause({
+                type: HMSPlaylistType.video,
+                url: currentVideo.url,
+              });
+              if (videoTrack) {
+                await hmsActions.removeTrack(videoTrack.id);
+              }
+              if (audioTrack) {
+                await hmsActions.removeTrack(audioTrack.id);
+              }
+            }}
           >
             <CloseIcon />
           </Button>
