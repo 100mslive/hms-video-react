75a2c23084df359bb19990fff112d1343a3ba9b8
raviteja83 <ravi.theja@100ms.live>
2021-09-15 10:45:59 +0530

    fix: playlist item styling

--- src/components/Playlist/PlaylistItem.tsx
+++ src/components/Playlist/PlaylistItem.tsx
@@ -8,18 +8,12 @@ import { hmsUiClassParserGenerator } from '../../utils/classes';
 export interface PlaylistItemClasses {
   root?: string;
   titleContainer?: string;
-  truncate?: string;
-  selection?: string;
-  subtitle?: string;
 }
 
 const defaultClasses = {
   root:
     'flex justify-between items-center w-full px-3 py-3 hover:bg-gray-600 dark:hover:bg-gray-300 cursor-pointer',
   titleContainer: 'flex flex-col flex-1',
-  truncate: 'min-w-0 truncate',
-  subtitle: 'text-gray-500 dark:text-gray-500',
-  selection: 'text-brand-main',
 };
 
 export interface PlaylistItemProps {
@@ -56,8 +50,8 @@ export const PlaylistItem = ({
           variant="body"
           size="md"
           classes={{
-            rootBodyMd: `${styler('truncate')} ${
-              item.selected && highlightSelection ? styler('selection') : ''
+            rootBodyMd: `truncate ${
+              item.selected && highlightSelection ? 'text-brand-main' : ''
             }`,
           }}
         >
@@ -66,7 +60,7 @@ export const PlaylistItem = ({
         <Text
           variant="body"
           size="sm"
-          classes={{ rootBodySm: `${styler('truncate')}${styler('subtitle')}` }}
+          classes={{ rootBodySm: 'text-gray-500 dark:text-gray-500' }}
         >
           {item.metadata?.description}
         </Text>
@@ -77,7 +71,7 @@ export const PlaylistItem = ({
         <Text
           variant="body"
           size="sm"
-          classes={{ rootBodySm: styler('subtitle') }}
+          classes={{ rootBodySm: 'text-gray-500 dark:text-gray-500' }}
         >
           {formatDuration(item.duration)}
         </Text>
