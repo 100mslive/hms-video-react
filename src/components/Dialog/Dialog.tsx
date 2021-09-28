d1c5e61c0d67679bdf7ac720e86adcf4dbd2b172
Aavgeen Singh <aavgeen@frontrow.co.in>
2021-09-28 17:12:21 +0530

    :lipstick: :recycle: Exporting the default HMSDialog and converting props to be optional in various places.

---
+++ src/components/Dialog/Dialog.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Dialog, withStyles } from '@material-ui/core';
+
+
+export const HMSDialog = withStyles({
+  paper: {
+    borderRadius: '12px',
+    backgroundColor: 'inherit',
+  },
+})(Dialog);
