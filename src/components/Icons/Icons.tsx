ee81fd6f5884df008c929195b6d2b31a32d77e4a
Harsh Mathur <58532371+harshmathurx@users.noreply.github.com>
2021-10-12 18:25:41 +0530

    Changed the dimensions of the DocsIcon

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -662,9 +662,9 @@ export const ChatIcon = (props: React.SVGProps<SVGSVGElement>) => (
 
 export const DocsIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    width="16"
-    height="17" 
-    viewBox="0 0 16 17" 
+    width="24"
+    height="24" 
+    viewBox="0 0 24 24" 
     fill="none" 
     xmlns="http://www.w3.org/2000/svg"
     {...props}
